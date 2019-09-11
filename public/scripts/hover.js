026833b4627ef406e764377a804b3ba5ac2ed3a4
karlyabut <karleisenyabut@gmail.com>
2019-09-11 22:28:15 +0000

    Commented out jQuery (will reference in the future)

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  $(".tweet").hover(function() {
-    $(this).css("opacity", 1)
-    $(this).css("box-shadow", "10px 10px 5px #99ccff")
-  }, function() {
-    $(this).css("opacity", 0.8)
-    $(this).css("box-shadow", "")
-  })
-});
+// $(document).ready(function() {
+//   // --- our code goes here ---
+//   $(".tweet").hover(function() {
+//     $(this).css("opacity", 1)
+//     $(this).css("box-shadow", "10px 10px 5px #99ccff")
+//   }, function() {
+//     $(this).css("opacity", 0.8)
+//     $(this).css("box-shadow", "")
+//   })
+// });
