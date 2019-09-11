6246e738399984a70c75eb18a7a0d9fb07d7471e
karlyabut <karleisenyabut@gmail.com>
2019-09-11 20:59:36 +0000

    Implemented rendering tweets from the server (**Bug** every successful tweets appends the whole list of tweets)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,30 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" 
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" 
+//       },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 
 const createTweetElement = function(value) {
   const $tweet = `
@@ -60,4 +60,52 @@ const renderTweets = function(tweets) {
     })
   }
 }
-renderTweets(data);
+
+$(function() {
+  const $newTweetForm = $("#newTweetForm");
+  $newTweetForm.submit(function(event) {
+    event.preventDefault();
+    if($("#tweetTxtArea").val() === "") {
+      alert("Please write some tweet!");
+    } else if ($("#tweetTxtArea").val().length > 140) {
+      alert("TOO LONG!");
+    } else {
+      console.log("adgahdshdahdjjdh", $newTweetForm.val())
+      $.ajax({
+        url: "/tweets/",
+        type: "POST",
+        data: $newTweetForm.serialize()
+      })
+      .then(response => {
+        // $('#tweets-container').append(createTweetElement(response));
+        console.log("Success!", response);
+        loadTweets();
+      })
+    }
+  })
+})
+
+//let use async/await on this function!
+const loadTweets = async () => {
+  try {
+    const response = await $.ajax({
+      url: "http://localhost:8080/tweets",
+      type: "GET",
+      dataType: "JSON"
+    })
+    renderTweets(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//Helper function for a valid tweet
+// const isUserInputValid = inputTweet => {
+//   inputTweet !== null,
+//   inputTweet !== undefined,
+//   inputTweet !== ""
+// }
+
+loadTweets();
+
+// console.log("asdasdasd", data);
