a06171983f987bcd138b05dd35a3e355b5d09cac
karlyabut <karleisenyabut@gmail.com>
2019-09-12 19:46:36 +0000

    Changed the color scheme of the whole app / **BUG** toggling compose tweet section

--- public/scripts/clickableElements.js
+++ public/scripts/clickableElements.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   //scroll to a specific element (with animation)
   $("#writeTweet").click(function() {
+    $(".new-tweet").slideDown("slow");
     $("html, body").animate({
       scrollTop: $(".container").offset().top - 140
     }, 500);
     //sets the focus to the text area
     $("#tweetTxtArea").focus();
   })
+  if($(".new-tweet") && !$("#tweetTxtArea").is(":focus")){
+    $(".new-tweet").slideUp();
+  }
 });
