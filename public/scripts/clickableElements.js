6fc3681c0c8bb4002c183548c4b4b12f77423b43
karlyabut <karleisenyabut@gmail.com>
2019-09-13 03:24:59 +0000

    Fixed toggling bug for creating a tweet

--- public/scripts/clickableElements.js
+++ public/scripts/clickableElements.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
   //scroll to a specific element (with animation)
   $("#writeTweet").click(function() {
-    $(".new-tweet").slideDown("slow");
+
+    //set our text area to empty if toggled
+    if(!$("#tweetTxtArea").val("")){
+      $("#tweetTxtArea").val("");
+    }
+
+    $(".new-tweet").slideToggle();
+    //animate our user to the text area
     $("html, body").animate({
       scrollTop: $(".container").offset().top - 140
     }, 500);
-    //sets the focus to the text area
+    //sets the focus to the text area / always set counter text to 140 if user toggles
     $("#tweetTxtArea").focus();
+    $(".counter").text("140");
   })
-  if($(".new-tweet") && !$("#tweetTxtArea").is(":focus")){
-    $(".new-tweet").slideUp();
-  }
 });
